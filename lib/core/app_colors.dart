88928a301a4930f1ccc98b10e3a33c010e82151e
mariam292 <eng_mariamabumusallm@outlook.com>
2026-08-30 17:29:11 +0300

    Initial project structure

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF4C0D1C);
+  
+  static const Color blackColor = Color(0xFF2D2D2D);
+
+  static const Color whiteColor = Colors.white;
+
+  static const Color color7A6E6B = Color(0xFF7A6E6B);
+
+  static const Color colorEADFD8 = Color(0xFFEADFD8);
+
+  static const Color backGroundColor = Color(0xFFFAF8F6);
+
+  static const Color colorA7736D = Color(0xFFA7736D); 
+
+  static const Color colorA66860 = Color(0xFFA66860);
+
+  static const Color colorC4A4A0 = Color(0xFFC4A4A0);
+
+  static const Color colorAC9160 = Color(0xFFAC9160);
+
+  static const Color color584141 = Color(0xFF584141);
+
+  static const Color colorD5C2C5 = Color(0xFFD5C2C5);
+
+  static const Color colorBCA89B = Color(0xFFBCA89B);
+
+  static const Color color685D50 = Color(0xFF685D50);
+
+  static const Color colorE6E2DB = Color(0xFFE6E2DB);
+
+  static const Color colorD1D5DB = Color(0xFFD1D5DB);
+  
+  static const Color colorFDFBF7 = Color(0xFFFDFBF7);
+
+  static const Color color5A3036 = Color(0xFF5A3036);
+  
+
+
+}
---
+++ lib/core/app_text_style.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const TextStyle style48Regular = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle style32Regular = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle style28Regular = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle style24Regular = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle style20Regular = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+  );
+
+  // --- Size 32 ---
+  static const TextStyle style32Bold = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle style32SemiBold = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle style32Medium = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style32Light = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w300,
+  );
+
+  // --- Size 28 ---
+  static const TextStyle style28Bold = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle style28SemiBold = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle style28Medium = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style28Light = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w300,
+  );
+
+  // --- Size 24 ---
+  static const TextStyle style24Bold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle style24SemiBold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle style24Medium = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style24Light = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w300,
+  );
+
+  // --- Size 20 ---
+  static const TextStyle style20Bold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle style20SemiBold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle style20Medium = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style20Light = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w300,
+  );
+
+  // --- Size 16 ---
+  static const TextStyle style16Bold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle style16SemiBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle style16Medium = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style16Regular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle style16Light = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w300,
+  );
+
+  // --- Size 14 ---
+  static const TextStyle style14Bold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle style14SemiBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle style14Medium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style14Regular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle style14Light = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+  );
+
+  // --- Size 12 ---
+  static const TextStyle style12Bold = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle style12SemiBold = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle style12Medium = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style12Regular = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle style12Light = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w300,
+  );
+}
