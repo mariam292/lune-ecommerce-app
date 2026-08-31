ad29f36800b9a0f51d5bfa41e6b97b13d2ca130f
Zahramohamed-eng <zahra05.mohamed@gmail.com>
2026-08-31 13:08:13 +0300

    Update imports and remove unnecessary whitespace
    Refactor imports and clean up unused code in ProductsView.

--- lib/features/home/presentation/widgets/products_view.dart
+++ lib/features/home/presentation/widgets/products_view.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:lune/app_colors.dart';
-import 'package:lune/AppFonts.dart';
+import 'package:lune/app_text_style.dart';
 
 class ProductsView extends StatelessWidget{
    ProductsView({super.key,required this.section_text,required this.onPressed,required this.products});
@@ -102,47 +102,16 @@ class ProductsView extends StatelessWidget{
        ],
         )
       
-      
-      
-      
-      
-      
-      
-      
-      
+   
       
          ],);
        }, ),
     ),
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 
     ],
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
      );
   }
 }
