d9b9c94db3bbf0cfeee33b7047cc9c0f1cd9eb55
Zahramohamed-eng <zahra05.mohamed@gmail.com>
2026-08-31 13:06:00 +0300

    Refactor imports in featured_picks.dart

--- lib/features/home/presentation/widgets/featured_picks.dart
+++ lib/features/home/presentation/widgets/featured_picks.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:lune/app_colors.dart';
-import 'package:lune/AppFonts.dart';
+import 'package:lune/app_text_style.dart';
 import 'package:lune/products_view.dart';
 
 class FeaturedPicks extends StatelessWidget {
