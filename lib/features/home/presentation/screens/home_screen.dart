0fb37d616181ff206643ed3f3e73769fe378c6fb
Zahramohamed-eng <zahra05.mohamed@gmail.com>
2026-08-31 13:03:30 +0300

    Refactor imports in home_screen.dart
    Updated import statements and corrected the import for app_text_style.dart.

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:lune/app_colors.dart';
-import 'package:lune/AppFonts.dart';
+import 'package:lune/app_text_style.dart';
 import 'package:lune/bottom_nav_bar.dart';
 import 'package:lune/collections.dart';
 import 'package:lune/featured_picks.dart';
