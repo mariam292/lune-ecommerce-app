2f00962feb1f85d1d02d8943b8b75b3862a2442d
nour <nourjalal351@gmail.com>
2026-09-09 01:18:19 +0200

    add authentication api

---
+++ lib/features/home/data/data_source/remote_data_source.dart
@@ -0,0 +1,81 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+
+class HomeRemoteData 
+
+{
+
+final  dio =Dio();
+
+Future<List> get_products()async
+{
+
+try {
+  final Response response =await dio.get('https://accessories-eshop.runasp.net/api/products');
+  
+  return response.data["items"];
+} on DioException catch (e) {
+   throw Exception(e.response?.data['message'] );
+}
+
+ 
+}
+ 
+
+
+Future<List> cart_products({required praductid}) async {
+    
+
+    try {
+      final Response response = await dio.post(
+        'https://accessories-eshop.runasp.net/api/cart/items'
+        ,data:{
+
+            "productId":praductid,
+  "quantity":1}
+  ,options: Options(headers: {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5ODEwMDc4Mi1mMTAxLTRjNzYtMzVkOS0wOGRmMGRhYTRmOTMiLCJqdGkiOiJjNWJhNzUwNi03NzM0LTQ3Y2EtYjUzMy0zNzIxNzM2MzgzYmMiLCJlbWFpbCI6Im1hcmlhbWFidW11c2FsbG01NUBnbWFpbC5jb20iLCJuYW1lIjoibWFyaWFtIGVtYWQiLCJyb2xlcyI6IiIsInBpY3R1cmUiOiIiLCJleHAiOjE3ODkxMDU0NDksImlzcyI6ImVzaG9wLm5ldCIsImF1ZCI6ImVzaG9wLm5ldCJ9.tRC5Ra3HL4qQypbaEI6eHmTLcI82T9cVx_gEEYezf0I',})
+        
+        
+        ); 
+         log("${response.data.toString()}");
+   return  response.data ;
+       
+          
+       
+     
+    } on DioException catch (e) {
+       
+        throw Exception (e.response?.data.toString() );
+    }
+  }
+
+ 
+
+Future<List> get_categories()async{
+   
+log("before res");
+
+ 
+  try {
+    final Response response =await dio.get("https://accessories-eshop.runasp.net/api/categories"
+    ,options: Options(headers: {'Authorization':  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5ODEwMDc4Mi1mMTAxLTRjNzYtMzVkOS0wOGRmMGRhYTRmOTMiLCJqdGkiOiJjNWJhNzUwNi03NzM0LTQ3Y2EtYjUzMy0zNzIxNzM2MzgzYmMiLCJlbWFpbCI6Im1hcmlhbWFidW11c2FsbG01NUBnbWFpbC5jb20iLCJuYW1lIjoibWFyaWFtIGVtYWQiLCJyb2xlcyI6IiIsInBpY3R1cmUiOiIiLCJleHAiOjE3ODkxMDU0NDksImlzcyI6ImVzaG9wLm5ldCIsImF1ZCI6ImVzaG9wLm5ldCJ9.tRC5Ra3HL4qQypbaEI6eHmTLcI82T9cVx_gEEYezf0I'}));
+    log(response.data.toString());
+     log("after res");
+     
+   
+    return response.data["categories"];     
+  } on Exception catch (e) {
+     throw Exception(e.toString());
+  }
+    
+ 
+}
+
+
+
+
+
+
+
+}
---
+++ lib/features/home/presentation/cubits/category_cubit.dart
@@ -0,0 +1,26 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nti_final_project/features/home/data/data_source/remote_data_source.dart';
+import 'package:nti_final_project/features/home/presentation/cubits/category_states.dart';
+
+class CategoryCubit extends Cubit<CategoryState> {
+  CategoryCubit() : super(CategoryInitialState());
+
+  HomeRemoteData homeRemoteData = HomeRemoteData();
+
+  Future<void> get_category() async {
+    emit(CategoryLoadingState());
+
+    await homeRemoteData.get_categories().then(
+      onError: (error) {
+        emit(CategoryFailureState(error.toString()));
+      },
+      (val) {
+        print(val);
+        emit(CategorySuccessState(val));
+      },
+    );
+  }
+}
